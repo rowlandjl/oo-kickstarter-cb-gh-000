d098309abd2ebd449ab29290a0e13aeebbaa30b8
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 15:19:00 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,13 @@
 class Backer
 
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
-  def back_project
-  end 
+  def back_project(project)
+  end
 
 end
